040b66408d26e346247b041f3c444f7d3407111b
Anooshakhalid <144916298+Anooshakhalid@users.noreply.github.com>
2026-04-19 18:26:47 +0500

    added influx setting

--- config/setting.py
+++ config/setting.py
@@ -1,2 +1,12 @@
 FRAME_LIMIT = 20   # how many samples per batch
 BATCH_PREFIX = "B"
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+INFLUX_URL = os.getenv("INFLUX_URL")
+INFLUX_TOKEN = os.getenv("INFLUX_TOKEN")
+INFLUX_ORG = os.getenv("INFLUX_ORG")
+INFLUX_BUCKET = os.getenv("INFLUX_BUCKET")
