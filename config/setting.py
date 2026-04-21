817a51d42bae67c92296d1447448c7de93fb2557
Anooshakhalid <144916298+Anooshakhalid@users.noreply.github.com>
2026-04-21 19:02:03 +0500

    updated the file

--- config/setting.py
+++ config/setting.py
@@ -1,10 +1,10 @@
-FRAME_LIMIT = 20   # how many samples per batch
-BATCH_PREFIX = "B"
-
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+FRAME_LIMIT = 20
+BATCH_PREFIX = "B"
 
 INFLUX_URL = os.getenv("INFLUX_URL")
 INFLUX_TOKEN = os.getenv("INFLUX_TOKEN")
