8d787fae0077e1773b1c9acdfbf18279acc17b37
Anooshakhalid <144916298+Anooshakhalid@users.noreply.github.com>
2026-04-22 20:45:08 +0500

    added the webcam logic

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import time
 
 from config.setting import FRAME_LIMIT
-from config.setting import INFLUX_URL, INFLUX_TOKEN, INFLUX_ORG, INFLUX_BUCKET
 from detection.model import get_detections
 from processing.analyzer import process
 from display.hmi import show
 from database.db import save_to_influx
 from utils.batch_manager import BatchManager
+from camera.capture import get_frame
+
 
 batch_manager = BatchManager()
 batch_id = batch_manager.new_batch()
@@ -15,21 +16,28 @@ frame_count = 0
 
 while True:
 
-    # STEP 1: fake detections
-    detections = get_detections()
+    # STEP 1: get REAL camera frame 
+    frame = get_frame()
 
-    # STEP 2: process logic
+    if frame is None:
+        print("Camera error")
+        continue
+
+    # STEP 2: run YOLO model 
+    detections = get_detections(frame)
+
+    # STEP 3: process logic
     result = process(detections, batch_id)
 
-    # STEP 3: save to InfluxDB (for Grafana)
+    # STEP 4: save to InfluxDB
     save_to_influx(result)
 
-    # STEP 4: show HMI
+    # STEP 5: show HMI
     show(result)
 
     print(result)
 
-    # STEP 5: batch control (CLEAN + CONFIG-BASED)
+    # STEP 6: batch control
     frame_count += 1
 
     if frame_count >= FRAME_LIMIT:
