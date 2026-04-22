81e4888b4a773be662b65890d937973d7b2c51ca
Anooshakhalid <144916298+Anooshakhalid@users.noreply.github.com>
2026-04-22 20:49:19 +0500

    added the txt to save the last batch_id

--- utils/batch_manager.py
+++ utils/batch_manager.py
@@ -1,8 +1,16 @@
 class BatchManager:
     def __init__(self):
-        self.counter = 1
+        try:
+            with open("batch.txt", "r") as f:
+                self.counter = int(f.read())
+        except:
+            self.counter = 1
 
     def new_batch(self):
         batch_id = f"B{self.counter:03d}"
         self.counter += 1
+
+        with open("batch.txt", "w") as f:
+            f.write(str(self.counter))
+
         return batch_id
