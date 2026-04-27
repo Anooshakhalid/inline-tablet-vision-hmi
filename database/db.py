2cd76367f373f37ab34730f14bc25b17f81b663d
Anooshakhalid <144916298+Anooshakhalid@users.noreply.github.com>
2026-04-27 18:10:05 +0500

    updated the logic

--- database/db.py
+++ database/db.py
@@ -17,20 +17,27 @@ write_api = client.write_api()
 # SAVE FUNCTION
 # =========================
 def save_to_influx(result):
+    try:
+        point = (
+            Point("tablet_detection")
+            .tag("batch_id", str(result["batch_id"]))
+            .tag("status", str(result["status"]))  
+            .field("tablet_count", int(result["total"]))
+            .field("normal", int(result["normal"]))
+            .field("chipped", int(result["chipped"]))
+            .field("capped", int(result["capped"]))
+            .time(datetime.utcnow(), WritePrecision.NS)  
+        )
 
-    point = (
-        Point("tablet_detection")
-        .tag("batch_id", result["batch_id"])
-        .field("tablet_count", int(result["total"]))
-        .field("normal", int(result["normal"]))
-        .field("chipped", int(result["chipped"]))
-        .field("capped", int(result["capped"]))
-        .field("status", result["status"])
-        .time(datetime.fromisoformat(result["time"]), WritePrecision.NS)
-    )
+        write_api.write(
+            bucket=INFLUX_BUCKET,
+            org=INFLUX_ORG,
+            record=point
+        )
 
-    write_api.write(
-        bucket=INFLUX_BUCKET,
-        org=INFLUX_ORG,
-        record=point
-    )
+        write_api.flush() 
+
+        print("Data successfully written to InfluxDB")
+
+    except Exception as e:
+        print("Error writing to InfluxDB:", e)
