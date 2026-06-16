65a6f3bd215939f73f1b1af981baa7b5eba722f3
Anoosha Khalid <anooshakhalid999@gmail.com>
2026-06-16 22:33:43 +0500

    corrected the cam index

--- pi_inference.py
+++ pi_inference.py
@@ -65,7 +65,7 @@ def send_log(event, data):
 # =====================
 # CAMERA
 # =====================
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
